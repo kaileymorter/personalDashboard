ad39b9555e77089cd1d53adf12d8ca12c93ce956
Spartacus <k_morter@outlook.com>
2020-06-16 23:27:37 -0600

    added task-list block

--- assets/js/personal.js
+++ assets/js/personal.js
@@ -158,3 +158,22 @@ var displaySearchedNews = function(searchData, searchTerm){
 getNews();
 newsSearchForm.addEventListener("submit", newsSubmitHandler);
 //END OF NEWS JS//
+
+//START OF TASK LIST JS//
+$('.save-btn').on('click', function(){
+    var taskNumber = $(this).attr('id');
+    var task = $(this).siblings('div.task-div').children("input").val();
+  
+    localStorage.setItem(taskNumber, task);
+  })
+
+  $("#task1").children("input").val(localStorage.getItem("task-1"));
+
+  $("#task2").children("input").val(localStorage.getItem("task-2"));
+  
+  $("#task3").children("input").val(localStorage.getItem("task-3"));
+  
+  $("#task4").children("input").val(localStorage.getItem("task-4"));
+  
+  $("#task5").children("input").val(localStorage.getItem("task-5"));
+  //END OF TASK LIST JS//
